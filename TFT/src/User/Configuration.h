d16a517f1828f2c04375c8dbeeaa29e9ba0b353f
eclenic <65027656+eclenic@users.noreply.github.com>
2020-08-21 21:05:15 +0100

    Update Configuration.h

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -75,7 +75,7 @@
  *  show banner text at the top of the TFT in Marlin Mode.
  */
 #define ST7920_SHOW_BANNER  true //to enabled: true | to disabled: false
-#define HD44780_SHOW_BANNER true 
+#define HD44780_SHOW_BANNER true
 /**
  * Run Marlin Mode in Fullscreen
  *
@@ -157,8 +157,8 @@
 
 //                         PLA      PETG      ABS
 #define PREHEAT_LABELS   {"PLA", "PETG", "ABS", "TPU"}
-#define PREHEAT_HOTEND   {200,   250,    230,   230}
-#define PREHEAT_BED      {60,    70,     100,   50}
+#define PREHEAT_HOTEND   {205,   235,    230,   230}
+#define PREHEAT_BED      {60,    75,     100,   50}
 
 #define HEAT_MAX_TEMP    {275,       275,       275,       275,       275,       275,       150,    60}   //max temperature can be set
 #define HEAT_SIGN_ID     {"T0:",     "T1:",     "T2:",     "T3:",     "T4:",     "T5:",     "B:",   "C:"}
@@ -191,9 +191,9 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 235
-#define Y_MAX_POS 235
-#define Z_MAX_POS 250
+#define X_MAX_POS 350
+#define Y_MAX_POS 350
+#define Z_MAX_POS 400
 
 // Specify a pause position as { X, Y, Z_raise }
 #define NOZZLE_PAUSE_RETRACT_LENGTH 15   // (mm)
@@ -221,20 +221,20 @@
 /**
  * Enable Unified Bed Leveling options.
  *  Will attempt to auto detect and enable specific UBL options.
- * 
+ *
  * WARNING - If you're not sure, leave on auto-detect or disabled.
  *           UBL has extra options other leveling systems might not have.
- * 
+ *
  * Options:  0: Disabled    1: Enabled    2: Auto-detect [default]
- * 
+ *
  */
 #define ENABLE_UBL_VALUE 2
 
 /**
  * Enable friendly probe offset language.
- * 
+ *
  * Decrease/increase and "-" & "+" icons are replaced with down/up and friendly icons
- * 
+ *
  * Options:  0: Disabled    1: Enabled
  */
 #define FRIENDLY_PROBE_OFFSET_LANGUAGE 1
